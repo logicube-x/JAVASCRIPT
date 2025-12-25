874a7c92a0d713d7221d56d0a8434335afc028bc
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2025-12-25 10:54:36 +0530

    Add files via upload

---
+++ array1.js
@@ -0,0 +1,38 @@
+console.log("hello")
+const myArray=[0,1,7,3,4,5,"a"]
+const myHeros=["ironman","batman","superman"]
+// const mynewArray=new Array("Arsh","Zeba")
+let index=2;
+// console.log(myArray);
+// console.log(myArray[3]);
+console.log(`An index of ${index} returns the value ${myArray.at(index)} `);
+// console.log(mynewArray);
+
+// console.log(`An index of ${index} returns the value ${myArray.at(index)}`)
+
+// console.log(myArray);
+// myArray.unshift(7)
+// console.log(myArray);
+// myArray.shift()
+// console.log(myArray);
+// myArray.shift(0)
+// console.log(myArray);
+
+console.log(myArray);
+const tempArr=myArray.slice(2,6)
+console.log(tempArr);
+
+const aa=[1,2,3,4,5,6]
+// console.log(aa.slice(2,5));
+// console.log(aa);
+// console.log(aa.slice(3));
+// console.log(aa.slice(1));
+
+// console.log(aa.slice(1));
+// console.log(1,3);
+// console.log(aa.slice(1,3));
+
+// aa.splice(1,3,"Arsh")
+// console.log(aa);
+console.log(aa);
+
---
+++ array2.js
@@ -0,0 +1,38 @@
+// const marvelHeros=["ironman","hulk","thor"]
+// const dc_heros=["batman","superman","flash"]
+
+// // marvelHeros.push(dc_heros)
+// // console.log(marvelHeros);
+
+
+// // console.log(marvelHeros[3][1]);  /*not good practise */
+
+// const new_heros=marvelHeros.concat(dc_heros)
+
+// // console.log(new_heros);
+// // new variable is assigned and then concat fns is used
+
+// const fruits=["apple","banana","grapes"]
+// const vegetables=["potato","brinjal","tomato"]
+
+// const union=[...dc_heros,...marvelHeros]
+// console.log(union);
+// // spread is most used 
+
+// const tempArray=[1,2,3,[4,5,[4,9,[1]]]]
+
+// const realArr=tempArray.flat(Infinity)
+// console.log(realArr);
+
+// console.log(Array.from("Arsh"))
+
+// score1=100
+// score2=200
+// score3=300
+// const sortedscore=Array.of(score1,score2,score3)
+// console.log(sortedscore)
+
+console.log(Array.isArray("3"));
+console.log(Array.isArray[3]);
+console.log(Array.isArray([]));
+console.log(Array.isArray(new Array(4,8)));
