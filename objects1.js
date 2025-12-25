f61b5fdc74147c42435c5c6f82b732e4f2d483fd
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2025-12-25 10:48:55 +0530

    Add files via upload

---
+++ objects1.js
@@ -0,0 +1,15 @@
+// Object.create
+
+const jsUser={
+    name:"Arsh",
+    "full name":"Arsh Mansuri",
+    age:19,
+    location:"Vadodara",
+    email:"arsh.amin.may.2006@gmail.com",
+    isLoggedIn: false,
+    lastloginDays:["Monday","Wednesday"]
+}
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+console.log(jsUser["full name"])
+console.log(jsUser.name);
