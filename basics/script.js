00fc002e5dbca0deafc60b79391b40c6231d5426
logicube-x <arsh.amin.may.2006@gmail.com>
2026-07-03 23:11:44 +0530

    basics of js

---
+++ basics/script.js
@@ -0,0 +1,22 @@
+let score=10;
+
+score=34;
+
+console.log(score);
+
+let userName="Arsh";
+
+console.log(userName);
+
+let colors=["violet","blue","green","red"]
+console.log(colors);
+
+let user={
+    name:"Arsh",
+    age:20,
+    isLoggedIn:true,
+    email:"arsh@gmail.com",
+    education:"B.Tech",
+    branch:"CE"
+}
+console.log(user);
