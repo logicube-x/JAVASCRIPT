c2076e79c343da4b06b108bd3ec61e6cc5672cd5
logicube-x <arsh.amin.may.2006@gmail.com>
2026-07-04 23:45:30 +0530

    non premitive data types

---
+++ basics/nonPremitive.js
@@ -0,0 +1,24 @@
+//  Objects ,
+
+let obj={}
+console.log(obj);
+console.log(typeof(obj));
+
+let user={
+    name:"Arsh Mansuri",
+    isLoggedIn:true,
+    mail:"arsh@gmail.com",
+    gender:"male",
+    city:"vadodara",
+    cgpa:8.1
+
+}
+console.log(user);
+console.log(typeof(user));
+
+let person={
+    "full name":"Arsh Mansuri",
+    age:20
+}
+
+console.log(person["full name"]);
