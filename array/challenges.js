96b895872304942dfa9f2250b5e500880c1cc1da
logicube-x <arsh.amin.may.2006@gmail.com>
2026-07-05 19:08:18 +0530

    Solved array challenges

---
+++ array/challenges.js
@@ -0,0 +1,33 @@
+// 1
+
+let teaFlavours=[`green tea`,`black tea`,`oolong tea`]
+
+let firstTea=teaFlavours[0];
+console.log(firstTea);
+
+// 2
+
+let cities=["london","Tokyo","Paris","New York"]
+let myCity=cities[2];
+console.log(myCity);
+// 3
+
+let teaTypes=["herbel tea","white tea","masala chai"]
+
+teaFlavours[1]="Jasmine tea";
+
+console.log(teaFlavours);
+
+// 4
+
+let citiesVisited=["Mumbai","Sydney"]
+citiesVisited.push("Berlin")
+console.log(citiesVisited);
+
+// 5
+let teaorders=["chai","matcha","ice tea","earl grey"]
+
+let lastOrder=teaorders.pop()
+console.log(lastOrder);
+
+// 6
