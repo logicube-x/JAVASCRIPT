48135f54b60c5a4b8f2e33c2732fa66257e3f8d5
logicube-x <arsh.amin.may.2006@gmail.com>
2026-07-05 12:33:06 +0530

    conditionals

---
+++ basics/conditionals.js
@@ -0,0 +1,47 @@
+let a=2;
+let b=3;
+
+if(a<b) console.log("b is greater than a");
+
+let n1=5;
+let n2=10;
+
+if(n2>n1){
+    console.log("n2 is greater than n1");
+}
+
+else{
+    console.log("n1 is greater");
+}
+
+let s1="Arsh"
+let s2="Ash"
+
+if(s1===s2){
+    console.log("Select diff name");
+}
+else{
+    console.log("you are good to continue");
+}
+
+
+let score=100;
+
+if(typeof score==="number"){
+    console.log("The score is valid ");
+}
+else{
+    console.log("not a number");
+    
+}
+
+// array is empty or not 
+
+let arr=[]
+
+if(arr.length===0){
+    console.log("Array is empty");
+}
+else{
+    console.log("Array is not empty ");
+}
