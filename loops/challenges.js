5624cebbe137b5c72b76fb2d6cbf2c4d4d5010e3
logicube-x <arsh.amin.may.2006@gmail.com>
2026-07-11 13:06:22 +0530

    solved two challenges on while loop

--- loops/challenges.js
+++ loops/challenges.js
@@ -8,3 +8,41 @@ while(i<=5){
     i++;
 }
 console.log(sum);
+
+// 2
+let j=5;
+countDown=[];
+
+while(j>=1){
+    countDown.push(j);
+    j--;
+}
+
+console.log(countDown);
+
+// 3
+
+// let tea;
+// let teaCollection=[]
+
+// do{
+//     tea=prompt(`Enter the name of the tea: (write stop to close)`)
+   
+//     if(tea!=="stop"){
+//         teaCollection.push(tea);
+//     }
+
+// } while(tea!=="stop")
+
+// 4
+
+let k=1;
+let total=0;
+
+do{
+total+=k;
+k++;
+}while(k<=3)
+
+
+console.log(total);
