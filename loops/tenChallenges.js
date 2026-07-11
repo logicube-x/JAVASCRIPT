0d0e290bc58787bbadd948532eafb808d0953bc8
logicube-x <arsh.amin.may.2006@gmail.com>
2026-07-11 20:39:21 +0530

    completed the first challenge problem

---
+++ loops/tenChallenges.js
@@ -0,0 +1,13 @@
+// 1
+let teas=["green tea","black tea","chai","oolong tea"]
+let selectedTeas=[]
+for(let i=0;i<teas.length;i++){
+    
+    if(teas[i]==="chai") break;
+
+    else{
+        selectedTeas.push(teas[i])
+    }
+}
+
+console.log(selectedTeas);
