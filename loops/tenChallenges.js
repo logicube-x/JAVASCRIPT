9c37c5680a10c807be71bb9c2bce578709974cc4
logicube-x <arsh.amin.may.2006@gmail.com>
2026-07-14 07:19:31 +0530

    solved one for problem on iteration through for loop on array

--- loops/tenChallenges.js
+++ loops/tenChallenges.js
@@ -121,3 +121,15 @@ cty.forEach((sel)=>{
 })
 
 console.log(travelledcities);
+
+// 9
+
+let nums=[2,5,7,9];
+let doubledNums=[];
+
+for(let i=0;i<nums.length;i++){
+    if(nums[i]===7) continue;
+
+    doubledNums.push(nums[i]*2);
+}
+console.log(doubledNums);
