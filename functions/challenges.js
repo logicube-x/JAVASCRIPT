a8327d136eb4022d33d2144d656c60ec4900dfb0
logicube-x <arsh.amin.may.2006@gmail.com>
2026-07-14 15:24:45 +0530

    completed the second problem

--- functions/challenges.js
+++ functions/challenges.js
@@ -6,3 +6,20 @@ function makeTea(type){
 
 let teaOrder=makeTea("Green Tea")
 console.log(teaOrder);
+
+// 2
+
+function orderTea(teaTypes){
+
+    function orderConfirmed(){
+
+        return `Order confirmed for chai`
+    }
+
+    return orderConfirmed()
+
+}
+
+let teaa=orderTea()
+console.log(teaa);
+
